00de73eb4c10da439b71a6cbdda290eb828455c5
uribo <ur1b0.dev@gmail.com>
2023-08-03 18:28:00 +0900

    開閉検知後に一定時間のスリープを入れた

--- main.py
+++ main.py
@@ -40,14 +40,16 @@ while(True):
         with open(filename,"r") as f:
             # "2022/11/06 21:34:25,::rc=80000000:lq=15:ct=002B:ed=8xxxxxxxx:id=0:ba=3080:bt=0000"
             lines = f.readlines()[-1:][0][:-1]
-            print(lines)
-            # "bt=0001" or "bt=0000"
-            data = lines.split(",")[1].split(":")[-1:][0]
-            if log_old != lines:
-                # メッセージ送信
-                ret = send_line_message(conf["line_token"],"\n荷物が届きました")
-                print(f"line notifier response:{ret}")
-                log_old = lines
+        print(lines)
+        if log_old != lines:
+            # メッセージ送信
+            ret = send_line_message(conf["line_token"],"\n荷物が届きました")
+            print(f"line notifier response:{ret}")
+
+            # 検知休憩
+            time.sleep(10)
+            with open(filename,"r") as f:
+                log_old = f.readlines()[-1:][0][:-1]
 
     # 単発動作 or インターバル動作
     if conf["interval"] != 0:
